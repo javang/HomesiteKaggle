1ff322936b8cb2ec4bb86d42cf603eb90e597921
marcianomoreno <marciano_moreno@outlook.com>
2015-12-06 13:03:22 -0600

    Commit from VM. This is more detail.

--- svm_model.R
+++ svm_model.R
@@ -25,7 +25,7 @@ require(yaml)
 conf = yaml.load_file("project.conf")
 standardInit()
 
-train_svm <- function(dataset){
+train_svm <- function(dataset, probability){
     model <- svm(QuoteConversion_Flag~., 
                  data=dataset, 
                  method="C-classification", 
@@ -33,7 +33,7 @@ train_svm <- function(dataset){
                  cost = 1,
                  gamma = 0.00729927,
                  na.action = na.omit, 
-                 probability = TRUE)
+                 probability = probability)
     return (model)
 }
 
@@ -43,8 +43,8 @@ params <- tune.svm(QuoteConversion_Flag~.,
                    gamma = 10^(-6:2), cost = 10^(-1:2))
 }
 
-evaluate_svm <- function(model, dataset, metric){
-    svm_prediction <- predict(model, dataset[,-1,with=FALSE], probability = TRUE)
+evaluate_svm <- function(model, dataset, probability, metric){
+    svm_prediction <- predict(model, dataset[,-1,with=FALSE], probability = probability)
     pred <- prediction(as.numeric(svm_prediction), as.numeric(dataset$QuoteConversion_Flag))
     tab <- table(pred = svm_prediction, true = dataset[,QuoteConversion_Flag])
     if(metric=="Diagonal"){
@@ -87,11 +87,11 @@ error_rate <- function(tab){
     fn = tab[1,2]
     return((fp+fn)/(tp+fp+tn+fn))
 }
-createLearningCurvesSVM = function(dataPointsFractions, numberOfFeatures, evaluation_metric) {
-    dataDir = conf$general$data_directory
-    vizDir = conf$general$visualizations_directory
-    load(file.path(dataDir, conf$input$fn_reduced_training)) # loads modelTrainData
-    load(file.path(dataDir, conf$input$fn_reduced_testing)) # loads modelTestData
+createLearningCurvesSVM = function(fnTrainingDataset, fnTestingDataset, 
+                                   dataPointsFractions, numberOfFeatures, probability, evaluation_metric, vizDir) {
+    loginfo("Creating learning curves for SVM")
+    load(fnTrainingDataset)
+    load(fnTestingDataset)
     modelTrainData = selectFeatures(modelTrainData, numberOfFeatures) 
     modelTestData = selectFeatures(modelTestData, numberOfFeatures)
     
@@ -100,21 +100,21 @@ createLearningCurvesSVM = function(dataPointsFractions, numberOfFeatures, evalua
     numCurvePoints = length(dataPointsFractions)
     trainFs = rep(0, numCurvePoints)
     testFs = rep(0, numCurvePoints)
-    fnCurves = file.path(dataDir, paste0("LearningCurves.SVM.", evaluation_metric, ".", numberOfFeatures, ".Features.txt"))
+    fnCurves = file.path(vizDir, paste0("LearningCurves.SVM.", evaluation_metric, ".", numberOfFeatures, ".Features.txt"))
     fileConn = file(fnCurves, "w")
     writeLines(c(paste("PointsFraction", "TrainFMeasure", "TestFMeasure",sep=",")), fileConn)
     close(fileConn)
     for (i in c(1:numCurvePoints)) {
         indices = randomSelect(nrows, dataPointsFractions[i])
         loginfo(paste("Creating a SVM model using",length(indices), "datapoints"))
-        model <- train_svm(modelTrainData[indices, ])
+        model <- train_svm(modelTrainData[indices, ], probability)
         # evaluate the model on the data used to create it
         #trainF = evaluateLogisticRegression(model, modelTrainData[indices,]) 
         #trainFs[i] = trainF
-        trainF = evaluate_svm(model, modelTrainData[indices, ], evaluation_metric)
+        trainF = evaluate_svm(model, modelTrainData[indices, ], probability, evaluation_metric)
         trainFs[i] = trainF
 
-        testF <- evaluate_svm(model, modelTestData, evaluation_metric)
+        testF <- evaluate_svm(model, modelTestData, probability, evaluation_metric)
         testFs[i] = testF
         fileConn = file(fnCurves, "at")
         writeLines(c(paste(dataPointsFractions[i], trainF, testF, sep=",")), fileConn)
@@ -135,21 +135,78 @@ createLearningCurvesSVM = function(dataPointsFractions, numberOfFeatures, evalua
     dev.off()
 }
 
-svm_learning_curves <- function(){
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),10, "Diagonal")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),20, "Diagonal")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),30, "Diagonal")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),40, "Diagonal")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),50, "Diagonal")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),60, "Diagonal") 
+svm_learning_curves_diagonal <- function(fnTrainingDataset, fnTestingDataset, vizDir){
+
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),10, "Diagonal", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),20, "Diagonal", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),30, "Diagonal", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),40, "Diagonal", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),50, "Diagonal", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),60, "Diagonal", vizDir) 
 }
 
-svm_learning_curves_fscore <- function(){
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),10, "F-measure")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),20, "F-measure")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),30, "F-measure")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),40, "F-measure")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),50, "F-measure")
-    createLearningCurvesSVM(c(0.1, 0.15, 0.2, 0.25, 0.3),60, "F-measure")
+svm_learning_curves_fscore <- function(fnTrainingDataset, fnTestingDataset, vizDir){
+
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.01, 0.015, 0.02, 0.025, 0.03),10, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),10, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),20, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),30, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),40, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),50, "F-measure", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),60, "F-measure", vizDir)
 }
 
+svm_learning_curves_error_rate <- function(fnTrainingDataset, fnTestingDataset, vizDir){
+    
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.01, 0.015, 0.02, 0.025, 0.03),10, "Error rate", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.01, 0.015, 0.02, 0.025, 0.03),20, FALSE, "Error rate", vizDir)
+    
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),10, "Error rate", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),20, FALSE, "Error rate", vizDir)
+    
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),30, "Error rate", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),40, "Error rate", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),50, "Error rate", vizDir)
+    createLearningCurvesSVM(fnTrainingDataset, fnTestingDataset, c(0.1, 0.15, 0.2, 0.25, 0.3),60, "Error rate", vizDir)
+}
+
+svm_learning_curves_diagonal_rel0 <- function(){
+    dataDir = conf$general$data_directory
+    fnTrainingDataset <- file.path(dataDir, conf$input$fn_reduced_training) # loads modelTrainData
+    fnTestingDataset <- file.path(dataDir, conf$input$fn_reduced_testing) # loads modelTestData
+    vizDir = conf$general$visualizations_directory
+    svm_learning_curves_diagonal(fnTrainingDataset, fnTestingDataset, vizDir)
+    
+}
+
+svm_learning_curves_diagonal_rel2 <- function(){
+    dataDir = data_directory_submission2
+    fnTrainingDataset <- file.path(dataDir, conf$input$fn_reduced_training_submission2) # loads modelTrainData
+    fnTestingDataset <- file.path(dataDir, conf$input$fn_reduced_training_submission2) # loads modelTestData
+    vizDir = conf$general$visualizations_directory_release2
+    svm_learning_curves_diagonal(fnTrainingDataset, fnTestingDataset, vizDir)
+    
+}
+svm_learning_curves_fscore_rel0 <- function(){
+    dataDir = conf$general$data_directory
+    fnTrainingDataset <- file.path(dataDir, conf$input$fn_reduced_training) # loads modelTrainData
+    fnTestingDataset <- file.path(dataDir, conf$input$fn_reduced_testing) # loads modelTestData
+    vizDir <- conf$general$visualizations_directory
+    svm_learning_curves_fscore(fnTrainingDataset, fnTestingDataset, dataDir, vizDir)
+}
+
+svm_learning_curves_fscore_rel2 <- function(){
+    dataDir = conf$general$data_directory_submission2
+    fnTrainingDataset <- file.path(dataDir, conf$input$fn_reduced_training_submission2) # loads modelTrainData
+    fnTestingDataset <- file.path(dataDir, conf$input$fn_reduced_testing_submission2) # loads modelTestData
+    vizDir <- conf$general$visualizations_directory_release2
+    svm_learning_curves_fscore(fnTrainingDataset, fnTestingDataset, vizDir)
+}
+
+svm_learning_curves_error_rate_rel2 <- function(){
+    dataDir = conf$general$data_directory_submission2
+    fnTrainingDataset <- file.path(dataDir, conf$input$fn_reduced_training_submission2) # loads modelTrainData
+    fnTestingDataset <- file.path(dataDir, conf$input$fn_reduced_testing_submission2) # loads modelTestData
+    vizDir <- conf$general$visualizations_directory_release2
+    svm_learning_curves_fscore(fnTrainingDataset, fnTestingDataset, vizDir)
+}
