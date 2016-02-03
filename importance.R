946e3ac32c6b92a39cd61af22e30fa5929442071
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2016-02-02 22:33:49 -0800

    Added calculation of importance using various algorithms

--- importance.R
+++ importance.R
@@ -28,9 +28,12 @@
 
 # --------------------------------------------
 
+require(yaml)
+require(caret) # filterVarImp
+require(CORElearn)
 source("initializer.R") # standardInit
 source("data_processor2.R") # chiSquareImportance
-require(yaml)
+
 
 conf = yaml.load_file("project.conf")
 
@@ -38,10 +41,47 @@ standardInit()
 dataDir = conf$general$data_directory
 fnPreprocessedTrain = file.path(dataDir, conf$input$preprocessed_train)  
 loginfo(paste("Loading the preprocessed train dataset:",fnPreprocessedTrain))
-load(fnPreprocessedTrain) # loads the data table originalTrainData, but IT IS THE preprocessed one
+load(fnPreprocessedTrain) # loads the data table as the object originalTrainData, but IT IS THE already preprocessed one
 
 # Calculate importance according to the chi-squared test
-chisqImportances = getChiSquaredImportance(originalTrainData, trainingFraction=0.01)
+# chisqImportances = getChiSquaredImportance(originalTrainData, trainingFraction=0.4)
+# df = data.frame(FeatureName=rownames(chisqImportances),Importance=chisqImportances$attr_importance)
+# write.csv(df, file.path(dataDir, "ChiSquareImportance.csv"))
+
+# Calculate importance of the categorical featuers according to the AUC for each of the features
+# factorColumns = get_factor_features(originalTrainData[,-1, with=FALSE]) # -1 means that QuoteConversion_Flag is removed
+# factorColumnNames = names(originalTrainData)[factorColumns]
+# factorsDataTable = originalTrainData[, c(factorColumnNames), with=FALSE]
+# outcomes = originalTrainData$QuoteConversion_Flag
+# fvi = filterVarImp(factorsDataTable, outcomes)
+# aucImportances = fvi[order(fvi$X0, decreasing = TRUE),]
+# df = data.frame(FeatureName=rownames(aucImportances),Importance=aucImportances$X0)
+# write.table(df, file.path(dataDir, "AucImportance.csv"), sep=',', row.names=FALSE)
+
+# Calculate importance according to the Relief algorithm
+reliefImportance = attrEval(QuoteConversion_Flag ~ ., originalTrainData,
+                            estimator="Relief", ReliefIterations=5)
+reliefImportance = reliefImportance[order(reliefImportance, decreasing = TRUE)]
+df = data.frame(FeatureName=names(reliefImportance),Importance=reliefImportance)
+write.table(df, file.path(dataDir, "reliefImportance.csv"), sep=',', row.names=FALSE)
 
 
+# Calculate importance according to the GainRatio algorithm
+gainRatioImportance = attrEval(QuoteConversion_Flag ~ ., originalTrainData, estimator="GainRatio")
+gainRatioImportance = gainRatioImportance[order(gainRatioImportance, decreasing = TRUE)]
+df = data.frame(FeatureName=names(gainRatioImportance),Importance=gainRatioImportance)
+write.table(df, file.path(dataDir, "gainRatioImportance.csv"), sep=',', row.names=FALSE)
 
+
+# Calculate importance according to the Gini-index measure
+giniImportance = attrEval(QuoteConversion_Flag ~ ., originalTrainData, estimator="Gini")
+giniImportance = giniImportance[order(giniImportance, decreasing = TRUE)]
+df = data.frame(FeatureName=names(giniImportance),Importance=giniImportance)
+write.table(df, file.path(dataDir, "giniImportance.csv"), sep=',', row.names=FALSE)
+
+
+# Calculate importance according to the reliefF importance that decreases exponentially with rank (Manhattan distance)
+ReliefFexpRankImportance = attrEval(QuoteConversion_Flag ~ ., originalTrainData, estimator="ReliefFexpRank")
+ReliefFexpRankImportance = ReliefFexpRankImportance[order(ReliefFexpRankImportance, decreasing = TRUE)]
+df = data.frame(FeatureName=names(ReliefFexpRankImportance),Importance=ReliefFexpRankImportance)
+write.table(df, file.path(dataDir, "ReliefFexpRankImportance.csv"), sep=',', row.names=FALSE)
